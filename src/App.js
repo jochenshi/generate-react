8b70151811c871e0e0bc3778bd3f02152456dac5
jochen <18362961791@163.com>
2017-09-15 18:19:03 +0800

    practise book

--- src/App.js
+++ src/App.js
@@ -6,12 +6,48 @@ import Home from './module/home/home'
 
 const About = React.createClass({
     propTypes: {
-        name: React.PropTypes.string.isRequired
+        name: React.PropTypes.string.isRequired,
+        age: React.PropTypes.number
+    },
+    getDefaultProps () {
+        return {
+            age: 20
+        }
     },
     render() {
         return (
-            React.DOM.h3(null, 'About' + this.props.name)
+            React.DOM.h3(null, 'About' + this.props.name + this.props.age)
         )
+    }
+})
+
+const TextCounter = React.createClass({
+    PropTypes: {
+        text: React.PropTypes.string
+    },
+    getDefaultProps () {
+        return {
+            text: ''
+        }
+    },
+    getInitialState () {
+        return {
+            text: this.props.text
+        }
+    },
+    _textChange (ev) {
+        this.setState({
+            text: ev.target.value
+        })
+    },
+    render () {
+        return React.DOM.div(null,
+            React.DOM.textarea({
+                defaultValue: this.props.text,
+                onChange: this._textChange
+            }),
+            React.DOM.span(null, this.state.text.length)
+            )
     }
 })
 
@@ -36,12 +72,18 @@ class App extends Component {
                     <li>
                         <Link to="/index">Index</Link>
                     </li>
+                    <li>
+                        <Link to="/text">Text</Link>
+                    </li>
                 </ul>
                 <div>
                     <Route path = "/about" render = { () => {
                         return React.createElement(About, {name: 'asd'})
                     }}/>
                     <Route path = "/index" component={Index}/>
+                    <Route path="/text" render = { () => {
+                        return React.createElement(TextCounter, {text: 'tom'})
+                    }}/>
                 </div>
                 <Home />
             </div>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import App from './App'
+import Excel from './module/excel/excel'
 
+const headers = ['Book', 'Auther', 'Language', 'Published', 'Sales'];
+const data = [
+    ['book1', 'authorddddddddd1', 'language1', 'published1', 'sales1'],
+    ['book2', 'author2', 'language2', 'published2', 'sales2'],
+    ['book3', 'authorddddd3', 'language3', 'published3', 'sales3'],
+    ['book4', 'author4', 'language4', 'published4', 'sales4'],
+    ['book5', 'author5', 'language5', 'published5', 'sales5'],
+    ['book6', 'author6', 'language6', 'published6', 'sales6'],
+
+];
 
 ReactDom.render(
     (
-        <Router>
-            <App/>
-        </Router>
+        <div>
+            <Excel headers = { headers } datas = { data }/>
+            <Router>
+                <App/>
+            </Router>
+        </div>
     ), document.getElementById('root')
 )
