b6756aaae992c22609b89bcfc389400bc24c8f80
jochen <18362961791@163.com>
2017-09-18 14:50:47 +0800

    excel test and export csv and json

--- src/module/excel/excel.js
+++ src/module/excel/excel.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 
 let Excel = React.createClass({
     displayName: 'Excel',
+    _preSearchData: null,
     propTypes: {
         headers: React.PropTypes.arrayOf(React.PropTypes.string)
     },
@@ -25,7 +26,8 @@ let Excel = React.createClass({
             data: sortData,
             sortBy: column,
             descending: descending,
-            edit: null
+            edit: null,
+            search: false
         });
         //console.log(event.target.cellIndex)
     },
@@ -49,7 +51,97 @@ let Excel = React.createClass({
             data: data
         })
     },
-    render () {
+    _toogleSearch () {
+        if (this.state.search) {
+            this.setState({
+                data: this._preSearchData,
+                search: false
+            });
+            this._preSearchData = null;
+        } else {
+            this._preSearchData = this.state.data;
+            this.setState({
+                search: true
+            })
+        }
+    },
+    _search (e) {
+        let needle = e.target.value.toLowerCase();
+        if (!needle) {
+            this.setState({
+                data: this._preSearchData
+            });
+            return
+        }
+        let idx = e.target.dataset.idx;
+        let searchData = this._preSearchData.filter( (row) => {
+            return row[idx].toString().toLowerCase().indexOf(needle) > -1;
+        });
+        this.setState({
+            data: searchData
+        })
+    },
+    _download (format, ev) {
+        let contents = format === 'json' ? JSON.stringify(this.state.data) :
+            this.state.data.reduce( (result, row) => {
+                return result + row.reduce( (rowresult, cell, idx) => {
+                    return rowresult + '"' + cell.replace(/"/g, '""') + '"' + (idx < row.length -1 ? ',' : '');
+                }, '') + '\n';
+            }, '');
+        let URL = window.URL || window.webkitURL;
+        let blob = new Blob([contents], {type: 'text/' + format});
+        ev.target.href = URL.createObjectURL(blob);
+        ev.target.download = 'data.' + format;
+    },
+    _renderSearch () {
+        if (!this.state.search) {
+            return null
+        }
+        return (
+            React.DOM.tr(
+                {onChange: this._search},
+                this.props.headers.map( (_ignore, idx) => {
+                    return React.DOM.td({key: idx},
+                        React.DOM.input({type: 'text', 'data-idx': idx})
+                    )
+                })
+            )
+        )
+    },
+    _renderToolbar () {
+        return (
+            React.DOM.div({className: 'toolbar'},
+                React.DOM.button(
+                    {
+                        onClick: this._toogleSearch
+                    },
+                    'Search'
+                ),
+                React.DOM.a(
+                    {
+                        onClick: this._download.bind(this, 'json'),
+                        href: 'data.json'
+                    },
+                    'Export JSON'
+                ),
+                React.DOM.a(
+                    {
+                        onClick: this._download.bind(this, 'csv'),
+                        href: 'data.csv'
+                    },
+                    'Export CSV'
+                )
+            )
+            /*React.DOM.button(
+                {
+                    onClick: this._toogleSearch,
+                    className: 'toolbar'
+                },
+                'search'
+            )*/
+        )
+    },
+    _renderTable () {
         return (
             React.DOM.table(null,
                 React.DOM.thead(null,
@@ -63,6 +155,7 @@ let Excel = React.createClass({
                     )
                 ),
                 React.DOM.tbody({onDoubleClick: this._showEditor},
+                    this._renderSearch(),
                     this.state.data.map( (row, rowidx) => {
                         return (
                             React.DOM.tr({key: rowidx},
@@ -88,6 +181,15 @@ let Excel = React.createClass({
                         )
                     })
                 )
+            )
+        )
+    },
+    render () {
+        return (
+            React.DOM.div(
+                null,
+                this._renderToolbar(),
+                this._renderTable()
             )
         )
     }
